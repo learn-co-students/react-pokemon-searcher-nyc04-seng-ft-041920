f3514358d7c8b092b94fef07ef8eb335cd2df17b
bashir2017 <bashiralhanshali@yahoo.com>
2020-07-17 15:33:45 -0400

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,35 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      displayFront: true
+    }
+    
+  }
+
+  toggleCard = () => {
+    this.setState(prevState => {
+      return {displayFront: !prevState.displayFront}
+    })
+  }
+
   render() {
+  const  {sprites, name, hp} = this.props.pokemon //object destructuring
     return (
-      <Card>
+      <Card onClick={this.toggleCard}>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img src={this.state.displayFront? sprites.front : sprites.back} alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {hp}
             </span>
           </div>
         </div>
