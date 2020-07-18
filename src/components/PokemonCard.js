2b369d9231df70d10f0808c51afdb6e53bfb2511
aleksarad <alexa.radevich1@gmail.com>
2020-07-18 15:50:17 -0400

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,34 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+
+  state = {
+    frontSprite: true
+  }
+
+  //when i do the below with the prevstate approach, it does not toggle
+  flipSprite = () => {
+    this.setState({
+      frontSprite: !this.state.frontSprite
+    })
+  }
+
   render() {
+    const { name, hp, sprites } = this.props.pokemon 
+    
     return (
       <Card>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img onClick={this.flipSprite} alt={name} src={this.state.frontSprite ? sprites.front : sprites.back} />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {hp}
             </span>
           </div>
         </div>
